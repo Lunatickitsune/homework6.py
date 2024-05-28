07fa52245b221b276356bacaefa74b6f9fb44562
valeriia <148711262+Lunatickitsune@users.noreply.github.com>
2024-05-29 03:53:25 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+my_dict = {'максим': 1998, 'анна': 2004, 'костя': 1995}
+print(my_dict)
+print(my_dict['анна'])
+my_dict['женя'] = 1978
+print(my_dict['женя'])
+my_dict.update({'саша': 2005, 'оля': 1995})
+print(my_dict)
+del my_dict['анна']
+print(my_dict)
+#вторая часть
+my_set = {1, 2, 'првиет', True, 2, 'чат', True}
+print(my_set)
+print(my_set.add(5))
+print(my_set.add('день'))
+print(my_set)
+print(my_set.discard(2))
+print(my_set)
+
+
+
+
+
